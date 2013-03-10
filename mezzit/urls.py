7a707d9ba8ac07888efe1938c90ce35c95b4e333
Stephen McDonald <steve@jupo.org>
2013-03-11 08:29:01 +1100

    Comments/links views for profiles, move user panel into nav, misc cleanups.

--- mezzit/urls.py
+++ mezzit/urls.py
@@ -6,4 +6,8 @@ urlpatterns = patterns("mezzit.views",
     url("^link/create/$", "link_create", name="link_create"),
     url("^link/(?P<slug>.*)/edit/$", "link_edit", name="link_edit"),
     url("^link/(?P<slug>.*)/$", "link_detail", name="link_detail"),
+    url("^users/(?P<username>.*)/comments/$", "user_comments",
+        name="user_comments"),
+    url("^users/(?P<username>.*)/links/$", "user_links",
+        name="user_links"),
 )
--- mezzit/views.py
+++ mezzit/views.py
@@ -1,20 +1,25 @@
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect
 from mezzanine.conf import settings
+from mezzanine.generic.models import ThreadedComment
 from mezzanine.utils.views import render, paginate
 
 from .models import Link
 from .forms import LinkForm
 
 
-def link_detail(request, slug):
-    link = get_object_or_404(Link, slug=slug)
-    return render(request, "link_detail.html", {"link": link})
+PAGING = (settings.ITEMS_PER_PAGE, settings.MAX_PAGING_LINKS)
+
+
+def link_detail(request, slug, template="link_detail.html"):
+    context = {"link": get_object_or_404(Link, slug=slug)}
+    return render(request, template, context)
 
 
 @login_required
-def link_create(request):
+def link_create(request, template="link_form.html"):
     link_form = LinkForm(request.POST or None)
     if request.method == "POST" and link_form.is_valid():
         link = link_form.save(commit=False)
@@ -22,25 +27,44 @@ def link_create(request):
         link.gen_description = False
         link.save()
         return redirect(link)
-    return render(request, "link_form.html", {"link_form": link_form})
+    context = {"link_form": link_form}
+    return render(request, template, context)
 
 
 @login_required
-def link_edit(request, slug):
+def link_edit(request, slug, template="link_form.html"):
     queryset = Link.objects.published(for_user=request.user)
     link = get_object_or_404(queryset, slug=slug)
     link_form = LinkForm(request.POST or None, instance=link)
     if request.method == "POST" and link_form.is_valid():
         link = link_form.save()
         return redirect(link)
-    return render(request, "link_form.html", {"link_form": link_form})
+    context = {"link_form": link_form}
+    return render(request, template, context)
 
 
-def link_list(request):
+def link_list(request, template="link_list.html"):
     page = request.GET.get("page", 1)
-    per_page = 20
-    max_links = settings.MAX_PAGING_LINKS
-    links = paginate(Link.objects.all(), page, per_page, max_links)
-    return render(request, "link_list.html", {"links": links})
+    links = paginate(Link.objects.all(), page, *PAGING)
+    context = {"links": links}
+    return render(request, template, context)
 
 
+def user_links(request, username, template="user_links.html"):
+    profile_user = get_object_or_404(User, username__iexact=username,
+                                     is_active=True)
+    page = request.GET.get("page", 1)
+    links = Link.objects.filter(user=profile_user)
+    links = paginate(links.order_by("-publish_date"), page, *PAGING)
+    context = {"links": links, "profile_user": profile_user}
+    return render(request, template, context)
+
+
+def user_comments(request, username, template="user_comments.html"):
+    profile_user = get_object_or_404(User, username__iexact=username,
+                                     is_active=True)
+    page = request.GET.get("page", 1)
+    comments = ThreadedComment.objects.filter(user=profile_user)
+    comments = paginate(comments.order_by("-submit_date"), page, *PAGING)
+    context = {"comments": comments, "profile_user": profile_user}
+    return render(request, template, context)
