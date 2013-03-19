d36f47fe86f5732e638b65f0c396317741ee8af8
Stephen McDonald <steve@jupo.org>
2013-03-19 20:45:39 +1100

    ajax post for ratings

--- mezzit/static/js/mezzit.js
+++ mezzit/static/js/mezzit.js
@@ -2,9 +2,12 @@ $(function() {
     $('.arrows a').click(function() {
         var arrow = $(this);
         var i = arrow.find('i').hasClass('icon-arrow-up') ? 1 : 0;
-        var form = arrow.parent().parent().find('form');
+        var container = arrow.parent().parent();
+        var form = container.find('form');
         form.find('input:radio')[i].checked = true;
-        form.submit();
+        $.post(form.attr('action'), form.serialize(), function(data) {
+            container.find('.score').text(data.rating_sum);
+        }, 'json');
         return false;
     });
 });
