294a82df4d1f75092ce138d48f870e513492e4ca
Stephen McDonald <steve@jupo.org>
2013-03-20 08:04:17 +1100

    Add login redirect handling to rating JS.

--- mezzit/static/js/mezzit.js
+++ mezzit/static/js/mezzit.js
@@ -1,12 +1,23 @@
 $(function() {
+    // Mezzit hides the radio buttons for +1 -1 ratings, and uses
+    // up/down arrow anchors. Attach click handlers to the arrow
+    // anchors that when clicked, check the relevant hidden radio
+    // button, and submits the form via AJAX. If the user is not
+    // authenticated, the JSON response will include a ``location``
+    // value to redirect to, otherwise it will contain the new
+    // rating score, which we update the page with.
     $('.arrows a').click(function() {
         var arrow = $(this);
-        var i = arrow.find('i').hasClass('icon-arrow-up') ? 1 : 0;
+        var index = arrow.find('i').hasClass('icon-arrow-up') ? 1 : 0;
         var container = arrow.parent().parent();
         var form = container.find('form');
-        form.find('input:radio')[i].checked = true;
+        form.find('input:radio')[index].checked = true;
         $.post(form.attr('action'), form.serialize(), function(data) {
-            container.find('.score').text(data.rating_sum);
+            if (data.location) {
+                location = data.location;
+            } else {
+                container.find('.score').text(data.rating_sum);
+            }
         }, 'json');
         return false;
     });
