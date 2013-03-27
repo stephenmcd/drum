b7d3f835e88fc83f5d24da83fa1e913730d7a8e1
Stephen McDonald <steve@jupo.org>
2013-03-28 08:09:11 +1100

    Added management command for creating links from rss feeds.

---
+++ mezzit/management/commands/poll_rss.py
@@ -0,0 +1,37 @@
+
+from datetime import datetime
+from time import mktime
+
+from django.core.management.base import BaseCommand
+from django.utils.timezone import get_default_timezone, make_aware
+from feedparser import parse
+
+from ...models import Link
+
+
+class Command(BaseCommand):
+
+    def handle(self, *urls, **options):
+        for url in urls:
+            for entry in parse(url).entries:
+                link = self.entry_to_link_dict(entry)
+                try:
+                    Link.objects.get(link=link["link"])
+                except Link.DoesNotExist:
+                    Link.objects.create(**link)
+
+    def entry_to_link_dict(self, entry):
+        link = {"title": entry.title, "user_id": 1}
+        try:
+            link["link"] = entry.summary.split('href="')[2].split('"')[0]
+        except IndexError:
+            link["link"] = entry.link
+        try:
+            publish_date = entry.published_parsed
+        except AttributeError:
+            pass
+        else:
+            publish_date = datetime.fromtimestamp(mktime(publish_date))
+            publish_date = make_aware(publish_date, get_default_timezone())
+            link["publish_date"] = publish_date
+        return link
