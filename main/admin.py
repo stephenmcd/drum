07ecf9c393dee634c364eea15d79fd00a22247c4
Stephen McDonald <steve@jupo.org>
2013-05-04 11:22:13 +1000

    Nicer link admin

--- main/admin.py
+++ main/admin.py
@@ -4,4 +4,13 @@ from mezzanine.core.admin import DisplayableAdmin
 from .models import Link
 
 
-admin.site.register(Link, DisplayableAdmin)
+class LinkAdmin(DisplayableAdmin):
+    list_display = ("id", "title", "status", "publish_date",
+                    "user", "comments_count", "rating_sum")
+    list_display_links = ("id",)
+    list_editable = ("title", "status")
+    list_filter = ("status", "user__username")
+    ordering = ("-publish_date",)
+
+
+admin.site.register(Link, LinkAdmin)
