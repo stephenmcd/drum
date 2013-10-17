6dbedc2cfc918dc823a07574de771e59dce65fa7
Stephen McDonald <steve@jupo.org>
2013-10-18 07:06:54 +1100

    Fix link admin fieldsets. Closes #3.

--- main/admin.py
+++ main/admin.py
@@ -5,12 +5,19 @@ from .models import Link
 
 
 class LinkAdmin(DisplayableAdmin):
-    list_display = ("id", "title", "status", "publish_date",
+
+    list_display = ("id", "title", "link", "status", "publish_date",
                     "user", "comments_count", "rating_sum")
     list_display_links = ("id",)
-    list_editable = ("title", "status")
+    list_editable = ("title", "link", "status")
     list_filter = ("status", "user__username")
     ordering = ("-publish_date",)
+
+    fieldsets = (
+        (None, {
+            "fields": ("title", "link", "status", "publish_date", "user"),
+        }),
+    )
 
 
 admin.site.register(Link, LinkAdmin)
