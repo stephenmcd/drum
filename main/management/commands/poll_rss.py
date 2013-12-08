8febff1065c67db1599f6b9bccd27f843981dd95
Stephen McDonald <steve@jupo.org>
2013-12-08 14:16:31 +1100

    Add an initial rating to new links in the rss seeder.

--- main/management/commands/poll_rss.py
+++ main/management/commands/poll_rss.py
@@ -2,9 +2,12 @@
 from datetime import datetime
 from time import mktime
 
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.timezone import get_default_timezone, make_aware
 from feedparser import parse
+
+from mezzanine.generic.models import Rating
 
 from ...models import Link
 
@@ -12,16 +15,22 @@ from ...models import Link
 class Command(BaseCommand):
 
     def handle(self, *urls, **options):
+        try:
+            user_id = User.objects.filter(is_superuser=1)[0].id
+        except IndexError:
+            return
         for url in urls:
             for entry in parse(url).entries:
                 link = self.entry_to_link_dict(entry)
+                link["user_id"] = user_id
                 try:
-                    Link.objects.get(link=link["link"])
+                    obj = Link.objects.get(link=link["link"])
                 except Link.DoesNotExist:
-                    Link.objects.create(**link)
+                    obj = Link.objects.create(**link)
+                    obj.rating.add(Rating(value=1, user_id=user_id))
 
     def entry_to_link_dict(self, entry):
-        link = {"title": entry.title, "user_id": 1, "gen_description": False}
+        link = {"title": entry.title, "gen_description": False}
         try:
             link["link"] = entry.summary.split('href="')[2].split('"')[0]
         except IndexError:
