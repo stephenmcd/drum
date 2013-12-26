72e48fb70370916a63dcc19dc8386c74ebe68161
Stephen McDonald <steve@jupo.org>
2013-12-26 11:00:50 +1100

    setup.*

---
+++ setup.py
@@ -0,0 +1,73 @@
+
+import os
+
+exclude = ["drum/project_template/dev.db",
+           "drum/project_template/local_settings.py"]
+exclude = dict([(e, None) for e in exclude])
+for e in exclude:
+    if e.endswith(".py"):
+        try:
+            os.remove("%sc" % e)
+        except:
+            pass
+    try:
+        with open(e, "r") as f:
+            exclude[e] = (f.read(), os.stat(e))
+        os.remove(e)
+    except:
+        pass
+
+from setuptools import setup, find_packages
+
+from drum import __version__ as version
+
+try:
+    setup(
+
+        name="Drum",
+        version=version,
+        author="Stephen McDonald",
+        author_email="stephen.mc@gmail.com",
+        description="A Reddit / Hacker News clone for Django.",
+        long_description=open("README.rst").read(),
+        license="BSD",
+        url="http://drum.jupo.org/",
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(),
+
+        install_requires=[
+            "mezzanine >= 3.0",
+        ],
+
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Web Environment",
+            "Framework :: Django",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.3",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+            "Topic :: Internet :: WWW/HTTP :: WSGI",
+            "Topic :: Software Development :: Libraries :: "
+                                                "Application Frameworks",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ])
+
+finally:
+    for e in exclude:
+        if exclude[e] is not None:
+            data, stat = exclude[e]
+            try:
+                with open(e, "w") as f:
+                    f.write(data)
+                os.chown(e, stat.st_uid, stat.st_gid)
+                os.chmod(e, stat.st_mode)
+            except:
+                pass
