1cfcd8a10efdfc51d0dd7114478fffa6661df261
Stephen McDonald <steve@jupo.org>
2014-11-09 22:03:39 +1100

    Add admin class for tags (aka keywords) when auto tagging enabled.

--- drum/links/admin.py
+++ drum/links/admin.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.contrib import admin
+from django.db import connection
+
 from mezzanine.core.admin import DisplayableAdmin
 from drum.links.models import Link
 
@@ -22,4 +25,33 @@ class LinkAdmin(DisplayableAdmin):
     )
 
 
+def delete_keywords(modeladmin, request, queryset):
+    ids = ",".join(map(str, queryset.values_list("id", flat=True)))
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM generic_assignedkeyword "
+                   "WHERE keyword_id IN (%s);" % ids)
+    cursor.execute("DELETE FROM generic_keyword WHERE id IN (%s);" % ids)
+
+
+class KeywordAdmin(admin.ModelAdmin):
+
+    ordering = ["title"]
+    list_display = ["id", "title", "slug"]
+    list_editable = ["title", "slug"]
+    actions = [delete_keywords]
+
+    class Media:
+        css = {"all": ["css/keywords.css"]}
+
+    def get_actions(self, request):
+        actions = super(KeywordAdmin, self).get_actions(request)
+        actions.pop("delete_selected")
+        return actions
+
+
 admin.site.register(Link, LinkAdmin)
+
+if getattr(settings, "AUTO_TAG", False):
+    from mezzanine.generic.models import Keyword
+    admin.site.register(Keyword, KeywordAdmin)
+
