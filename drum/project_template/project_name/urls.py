1786728a301028e6382ac6507d7b73fbc36d8f59
Park Minwoo <tebica@gmail.com>
2016-07-26 02:43:37 +0900

    update url format for new Django version 1.9

--- drum/project_template/project_name/urls.py
+++ drum/project_template/project_name/urls.py
@@ -1,16 +1,18 @@
 from __future__ import unicode_literals
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
 admin.autodiscover()
 
 
-urlpatterns = patterns("",
-    ("^admin/", include(admin.site.urls)),
-    ("^", include("drum.links.urls")),
-    ("^", include("mezzanine.urls")),
-)
+urlpatterns = [
+    url("^admin/", include(admin.site.urls)),
+    url("^", include("drum.links.urls")),
+    url("^", include("mezzanine.urls")),
+]
 
-# Adds ``STATIC_URL`` to the context.
+# Adds ``STATIC_URL`` to the context of error pages, so that error
+# pages can use JS, CSS and images.
+handler404 = "mezzanine.core.views.page_not_found"
 handler500 = "mezzanine.core.views.server_error"
